cc07f8681c92ab66870acf302a908a0d0d0d2926
Filip Woźniak <filip.wozniak98@wp.pl>
2023-01-29 01:55:09 +0100

    added icons in js

--- src/js/script.js
+++ src/js/script.js
@@ -1,40 +1,112 @@
-const btn = document.querySelector('.ok');
+const okBtn = document.querySelector('.ok');
 const input = document.querySelector('input');
 
-const API_LINK = "http://api.openweathermap.org/geo/1.0/direct?q=";
-const API_KEY = "&appid=b005763a5f5701376b641f6d866e7e64&units=metric";
+// Pollution=========
+const pollutionIcon = document.querySelector('.air-weather__img');
+const pollutionWord = document.querySelector('#air-quality');
 
+const pm10 = document.querySelector('#pm10');
+const pm25 = document.querySelector('#pm25');
+const co = document.querySelector('#co');
+const so2 = document.querySelector('#so2');
+
+//Weather==============
+const weatherIcon = document.querySelector('.weather-type__img');
+const weatherWord = document.querySelector('.weather-type__title');
+
+const temp = document.querySelector('#temp');
+const feel = document.querySelector('#feel');
+const hum = document.querySelector('#hum');
+const wind = document.querySelector('#wind');
+
+const city = document.querySelector('.adress__city');
+
+const API_LINK = 'http://api.openweathermap.org/geo/1.0/direct?q=';
+const API_KEY = '&appid=b005763a5f5701376b641f6d866e7e64&units=metric';
 
 // const API_LINK = "https://api.openaq.org/v1/measurements?"
 // const API_KEY2 = "&key=01c3c848e13647ee97e102809ba97b43";
 // const API_LINK3 = "https://api.openweathermap.org/data/2.5/weather?"
 
-
 const getCoordinates = () => {
     
-    const city = input.value || "Wrocław"
+    const city = input.value || 'Wrocław';
+  
 
-    fetch(API_LINK + city + API_KEY)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data[0])
-        const lat = `lat=${data[0].lat}`
-        const lon = `&lon=${data[0].lon}`
-        getWeather(lat,lon)
-    })
-    .catch(err => console.log('error', err));
-}
+	fetch(API_LINK + city + API_KEY)
+		.then(res => res.json())
+		.then(data => {
+			data[0];
+			const lat = `lat=${data[0].lat}`;
+			const lon = `&lon=${data[0].lon}`;
+			getAirPollution(lat, lon);
+            getWeather(lat,lon);
+		})
+		.catch(err => console.log('error', err));
+};
 
+const getAirPollution = (lat, lon) => {
+	const API_LINK2 = 'http://api.openweathermap.org/data/2.5/air_pollution?';
+	fetch(API_LINK2 + lat + lon + API_KEY)
+		.then(res => res.json())
+		.then(data => {
+
+			const locationIcon = document.createElement('img');
+			locationIcon.classList.add('adress__icon');
+			locationIcon.setAttribute('src', 'dist/img/icon_map.svg');
+			locationIcon.setAttribute('alt', 'location icon');
+			city.append(locationIcon, input.value);
+            // city.innerHTML = '';
+            // city.textContent = '';
+
+            if (data.list[0].main.aqi === 1) {
+                pollutionWord.textContent = "Dobra"
+                pollutionWord.style.color = "lime"
+                pollutionIcon.setAttribute('src', "dist/img/smile.png")
+
+            } else if (data.list[0].main.aqi === 2){
+                pollutionWord.textContent = "Średnia"
+                pollutionWord.style.color = "orange"
+                pollutionIcon.setAttribute('src', "dist/img/confused.png")
+            } else if (data.list[0].main.aqi >= 3){
+                pollutionWord.textContent = "Zła"
+                pollutionWord.style.color = "red"
+                pollutionIcon.setAttribute('src', "dist/img/mask.png")
+            }
+            // console.log(data.list[0].main.aqi);
+		});
+};
 
 const getWeather = (lat,lon) => {
-    const API_LINK2 = "http://api.openweathermap.org/data/2.5/air_pollution?";
-    fetch(API_LINK2 + lat + lon + API_KEY)
-     .then(res => res.json())
-     .then(data => {console.log(data)})
-    }
+    const API_LINK3 = "https://api.openweathermap.org/data/2.5/weather?";
+    fetch(API_LINK3 + lat + lon + API_KEY)
+    .then(res => res.json())
+    .then(data => {
+        
+        console.log(data.weather[0])
+        if(data.weather[0].id < 233 ){
+            weatherIcon.setAttribute("src", "dist/img/thunderstorm.png");
+            weatherWord.textContent = "Burza"
+        } else if (data.weather[0].id > 299 && data.weather[0].id < 500){
+            weatherIcon.setAttribute("src", "dist/img/drizzle.png");
+            weatherWord.textContent = "Mżawka"
+        } else if (data.weather[0].id >= 500 && data.weather[0].id < 600){
+            weatherIcon.setAttribute("src", "dist/img/rain.png");
+            weatherWord.textContent = "Deszczowo"
+        } else if (data.weather[0].id >= 600 && data.weather[0].id < 701){
+            weatherIcon.setAttribute("src", "dist/img/ice.png");
+            weatherWord.textContent = "Śnieg"
+        } else if (data.weather[0].id >= 701 && data.weather[0].id < 800){
+            weatherIcon.setAttribute("src", "dist/img/fog.png");
+            weatherWord.textContent = "Mgliśnie"
+        } else if (data.weather[0].id = 800){
+            weatherIcon.setAttribute("src", "dist/img/sun.png");
+            weatherWord.textContent = "Słonecznie"
+        } else {
+            weatherIcon.setAttribute("src", "dist/img/cloud.png");
+            weatherWord.textContent = "Pochmurno"
+        }
+    });
+}
 
-
-URL:  "http://api.openweathermap.org/data/2.5/air_pollution?lat=51.1089776&lon=17.0326689&appid=b005763a5f5701376b641f6d866e7e64&units=metric"
-
-btn.addEventListener('click',getCoordinates);
-// getCoordinates()
+okBtn.addEventListener('click', getCoordinates, getWeather);
