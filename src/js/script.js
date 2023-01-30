447dd4f4fc8589e7714211408ad41108a5927cf5
Filip Woźniak <filip.wozniak98@wp.pl>
2023-01-30 01:14:55 +0100

    added red colors in pollution cards and added enter event

--- src/js/script.js
+++ src/js/script.js
@@ -28,9 +28,12 @@ const API_KEY = '&appid=b005763a5f5701376b641f6d866e7e64&units=metric';
 // const API_KEY2 = "&key=01c3c848e13647ee97e102809ba97b43";
 // const API_LINK3 = "https://api.openweathermap.org/data/2.5/weather?"
 
+
+
 const getCoordinates = () => {
 	const city = input.value || 'Wrocław';
-
+    pm25.parentElement.style.backgroundColor = "rgba(213, 212, 212, 0.205)"
+    pm10.parentElement.style.backgroundColor = "rgba(213, 212, 212, 0.205)";
 	fetch(API_LINK + city + API_KEY)
 		.then(res => res.json())
 		.then(data => {
@@ -39,7 +42,6 @@ const getCoordinates = () => {
 			const lon = `&lon=${data[0].lon}`;
 			getAirPollution(lat, lon);
 			getWeather(lat, lon);
-			// getAirPollutionData();
 		})
 		.catch(err => console.log('error', err));
 };
@@ -47,19 +49,15 @@ const getCoordinates = () => {
 const getAirPollution = (lat, lon) => {
 	const API_LINK2 = 'http://api.openweathermap.org/data/2.5/air_pollution?';
 	fetch(API_LINK2 + lat + lon + API_KEY)
-		.then(res => res.json())
-		.then(data => {
+    .then(res => res.json())
+    .then(data => {
+        
             pm10.textContent = (data.list[0].components.pm10).toFixed(1) + " uq/m3"
             pm25.textContent = (data.list[0].components.pm2_5).toFixed(1) + " uq/m3"
             co.textContent = (data.list[0].components.co).toFixed(1) + " uq/m3"
             so2.textContent = (data.list[0].components.so2).toFixed(1) + " uq/m3"
-			const locationIcon = document.createElement('img');
-			locationIcon.classList.add('adress__icon');
-			locationIcon.setAttribute('src', 'dist/img/icon_map.svg');
-			locationIcon.setAttribute('alt', 'location icon');
-			city.append(locationIcon, input.value);
-			// city.innerHTML = '';
-			// city.textContent = '';
+
+			city.textContent = input.value.toUpperCase();
 			input.value = '';
 
 			if (data.list[0].main.aqi === 1) {
@@ -75,7 +73,17 @@ const getAirPollution = (lat, lon) => {
 				pollutionWord.style.color = 'red';
 				pollutionIcon.setAttribute('src', 'dist/img/mask.png');
 			}
-			// console.log(data.list[0].main.aqi);
+
+            if (data.list[0].components.pm2_5 >= 10) {
+                pm25.parentElement.style.backgroundColor = "rgba(207, 18, 49, 0.664)";
+            }if (data.list[0].components.pm10 >= 20){
+                pm10.parentElement.style.backgroundColor = "rgba(207, 18, 49, 0.664)";
+            }if (data.list[0].components.co >= 15000) {
+                co.parentElement.style.backgroundColor = "rgba(207, 18, 49, 0.664)";
+            }if (data.list[0].components.so2 >= 125) {
+                so2.parentElement.style.backgroundColor = "rgba(207, 18, 49, 0.664)";
+            }
+
 		});
 };
 
@@ -85,13 +93,11 @@ const getWeather = (lat, lon) => {
 		.then(res => res.json())
 		.then(data => {
             const mPerS = data.wind.speed
-            console.log(mPerS);
 			const kmPerH = (mPerS * 3.6).toFixed(1)
             temp.textContent = (data.main.temp).toFixed(1) + " ℃"
             feel.textContent = (data.main.feels_like).toFixed(1) + " ℃"
             hum.textContent = data.main.humidity + " %"
             wind.textContent = kmPerH + " km/h"
-            console.log(wind);
 			if (data.weather[0].id < 233) {
 				weatherIcon.setAttribute('src','dist/img/thunderstorm.png');
 				weatherWord.textContent = 'Burza';
@@ -118,4 +124,12 @@ const getWeather = (lat, lon) => {
 };
 
 
-okBtn.addEventListener('click', getCoordinates, getWeather);
+const pressEnter = (e) => {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        getCoordinates()
+    }
+}
+
+okBtn.addEventListener('click', getCoordinates);
+input.addEventListener('keyup', pressEnter);
