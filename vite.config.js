900964bc128677fcc82004f7abeb61eabce9d53b
Filip Woźniak <filip.wozniak98@wp.pl>
2024-11-29 12:57:01 +0100

    added overflow

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
+	base: '/AirPollutionApp/',
 	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, 'index.html'),
+			},
 		},
 	},
 });
